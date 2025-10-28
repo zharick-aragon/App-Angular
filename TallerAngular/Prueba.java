7a08f3a2ca598f5c85a38e5b3c9d3aa092e2862f
zharick-aragon <z.aragon@uniandes.edu.co>
2025-10-28 00:05:48 -0500

    Nuevo plazo

---
+++ TallerAngular/Prueba.java
@@ -0,0 +1,28 @@
+public class Prueba {
+    /* Eliminado de indez.html 
+    /*<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Mynewapp</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <app-root></app-root>
+</body>
+</html> */
+
+/*El main.ts se crea en automatico 
+ * 
+ * import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app/app-module';
+
+platformBrowser().bootstrapModule(AppModule, {
+  ngZoneEventCoalescing: true,
+})
+  .catch(err => console.error(err));
+ */
+
+}
